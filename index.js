50052fd8220046ca99e935e2f04296e08ddb7ade
jiriartem <77533807+jiriartem@users.noreply.github.com>
2022-09-01 23:25:47 +0200

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var app = express();
-var port = 3000;
-
-app.use('/', express.static('.'));
-app.listen(port);
-console.log(`Open browser to: http://localhost:${port}`);
