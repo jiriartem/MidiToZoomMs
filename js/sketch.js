610f2d079b3c1372706813993db32af7e4a23d46
jiriartem <77533807+jiriartem@users.noreply.github.com>
2022-10-25 22:02:27 +0200

    Update sketch.js
    Different behavior Floor Pod Plus - Live POD XT

--- js/sketch.js
+++ js/sketch.js
@@ -24,19 +24,53 @@ function onWebMidiEnabled() {
   // Add a listener on all the MIDI inputs that are detected
   WebMidi.inputs.forEach(input => {
 
-	if (input.name == '3- MIDISPORT Uno / 1x1 In'){
+	if ((input.name == '3- MIDISPORT Uno / 1x1 In') 
+		|| (input.name == 'Line 6 PODxt Live')){
 		// When a "note on" is received on MIDI channel 1, generate a random color start
 		input.channels[1].addListener("programchange", function(p) {
 		  //fill(random(255), random(255), random(255));
 		  //circle(random(width), random(height), 100);
-		  console.log('program X:' + p);
 		  if (!isNaN(p.value)){
-			SendPatch(p.value-1);
+			  console.log('program X:' + patchValue);
+			  var patchValue;
+			  var zoomLimit = 50;
+			  if (input.name == '3- MIDISPORT Uno / 1x1 In') {
+				  patchValue = p.value -1; //offset midi floor pod plus
+				  zoomLimit = 49;
+			  }
+			  else{
+				  patchValue = p.value;				   
+			  }
+				while (patchValue > zoomLimit){
+					patchValue -= zoomLimit;
+				}			  
+			  SendPatch(patchValue);
 		  }
-		  
+		  else{
+			  console.log('program Error');
+		  }
 		});
 	}
-
+	
+	if (input.name == 'ZOOM G Series'){
+		// When a "note on" is received on MIDI channel 1, generate a random color start
+		input.channels[1].addListener("programchange", function(p) {
+		  //fill(random(255), random(255), random(255));
+		  //circle(random(width), random(height), 100);
+		  if (!isNaN(p.value)){
+			  console.log('program X:' + patchValue);
+			  var patchValue = p.value -1;
+			  var zoomLimit = 44;
+			  while (patchValue > zoomLimit){
+					patchValue -= zoomLimit;
+				}
+				SendPatch(patchValue);
+		  }
+		  else{
+			  console.log('program Error');
+		  }
+		});
+	}
   });
 
 }
